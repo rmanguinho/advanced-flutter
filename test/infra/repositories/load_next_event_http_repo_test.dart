decd4cc77c051cb7508910720051b56617f0c1d7
Rodrigo Manguinho <rodrigo.manguinho@gmail.com>
2024-05-20 14:48:01 -0300

    apply clean code

--- test/infra/repositories/load_next_event_http_repo_test.dart
+++ test/infra/repositories/load_next_event_http_repo_test.dart
@@ -58,6 +58,12 @@ class HttpClientSpy implements Client {
   String responseJson = '';
   int statusCode = 200;
 
+  void simulateBadRequestError() => statusCode = 400;
+  void simulateUnauthorizedError() => statusCode = 401;
+  void simulateForbiddenError() => statusCode = 403;
+  void simulateNotFoundError() => statusCode = 404;
+  void simulateServerError() => statusCode = 500;
+
   @override
   void close() {}
 
@@ -178,31 +184,31 @@ void main() {
   });
 
   test('should throw UnexpectedError on 400', () async {
-    httpClient.statusCode = 400;
+    httpClient.simulateBadRequestError();
     final future = sut.loadNextEvent(groupId: groupId);
     expect(future, throwsA(DomainError.unexpected));
   });
 
   test('should throw SessionExpiredError on 401', () async {
-    httpClient.statusCode = 401;
+    httpClient.simulateUnauthorizedError();
     final future = sut.loadNextEvent(groupId: groupId);
     expect(future, throwsA(DomainError.sessionExpired));
   });
 
   test('should throw UnexpectedError on 403', () async {
-    httpClient.statusCode = 403;
+    httpClient.simulateForbiddenError();
     final future = sut.loadNextEvent(groupId: groupId);
     expect(future, throwsA(DomainError.unexpected));
   });
 
   test('should throw UnexpectedError on 404', () async {
-    httpClient.statusCode = 404;
+    httpClient.simulateNotFoundError();
     final future = sut.loadNextEvent(groupId: groupId);
     expect(future, throwsA(DomainError.unexpected));
   });
 
   test('should throw UnexpectedError on 500', () async {
-    httpClient.statusCode = 500;
+    httpClient.simulateServerError();
     final future = sut.loadNextEvent(groupId: groupId);
     expect(future, throwsA(DomainError.unexpected));
   });
