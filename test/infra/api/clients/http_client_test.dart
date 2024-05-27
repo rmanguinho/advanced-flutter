9b2c1f231e7968b9fa7c77eca1f5e670037333dd
Rodrigo Manguinho <rodrigo.manguinho@gmail.com>
2024-05-27 15:40:38 -0300

    ensure HttpClient request with correct url

--- test/infra/api/clients/http_client_test.dart
+++ test/infra/api/clients/http_client_test.dart
@@ -1,6 +1,7 @@
 import 'package:flutter_test/flutter_test.dart';
 import 'package:http/http.dart';
 
+import '../../../helpers/fakes.dart';
 import 'client_spy.dart';
 
 class HttpClient {
@@ -10,26 +11,32 @@ class HttpClient {
     required this.client
   });
 
-  Future<void> get() async {
-    await client.get(Uri());
+  Future<void> get({ required String url }) async {
+    await client.get(Uri.parse(url));
   }
 }
 
 void main() {
   late ClientSpy client;
   late HttpClient sut;
+  late String url;
 
   setUp(() {
     client = ClientSpy();
+    url = anyString();
     sut = HttpClient(client: client);
   });
 
   group('get', () {
     test('should request with correct method', () async {
-      await sut.get();
+      await sut.get(url: url);
       expect(client.method, 'get');
       expect(client.callsCount, 1);
     });
-  });
 
+    test('should request with correct url', () async {
+      await sut.get(url: url);
+      expect(client.url, url);
+    });
+  });
 }
