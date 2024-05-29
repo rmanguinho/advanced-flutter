e294b2e1f6e6a708293e860c340fcdb18fe2a46a
Rodrigo Manguinho <rodrigo.manguinho@gmail.com>
2024-05-29 16:32:59 -0300

    ensure HttpClient request with correct querystring

--- test/infra/api/clients/http_client_test.dart
+++ test/infra/api/clients/http_client_test.dart
@@ -11,15 +11,20 @@ class HttpClient {
     required this.client
   });
 
-  Future<void> get({ required String url, Map<String, String>? headers, Map<String, String?>? params }) async {
+  Future<void> get({ required String url, Map<String, String>? headers, Map<String, String?>? params, Map<String, String>? queryString }) async {
     final allHeaders = (headers ?? {})..addAll({ 'content-type': 'application/json', 'accept': 'application/json' });
-    final uri = _buildUri(url: url, params: params);
+    final uri = _buildUri(url: url, params: params, queryString: queryString);
     await client.get(uri, headers: allHeaders);
   }
 
-  Uri _buildUri({ required String url, Map<String, String?>? params }) {
+  Uri _buildUri({ required String url, Map<String, String?>? params, Map<String, String>? queryString }) {
     params?.forEach((key, value) => url = url.replaceFirst(':$key', value ?? ''));
     if (url.endsWith('/')) url = url.substring(0, url.length - 1);
+    if (queryString != null) {
+      url += '?';
+      queryString.forEach((key, value) => url += '$key=$value&');
+      url = url.substring(0, url.length - 1);
+    }
     return Uri.parse(url);
   }
 }
@@ -77,6 +82,11 @@ void main() {
       url = 'http://anyurl.com/:p1/:p2';
       await sut.get(url: url, params: { 'p3': 'v3' });
       expect(client.url, 'http://anyurl.com/:p1/:p2');
+    });
+
+    test('should request with correct queryStrings', () async {
+      await sut.get(url: url, queryString: { 'q1': 'v1', 'q2': 'v2' });
+      expect(client.url, '$url?q1=v1&q2=v2');
     });
   });
 }
