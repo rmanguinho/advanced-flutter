b182a96dcd45e034521b8abd7749935e585758a6
Rodrigo Manguinho <rodrigo.manguinho@gmail.com>
2024-05-29 16:34:53 -0300

    ensure HttpClient request with correct querystring and params

--- test/infra/api/clients/http_client_test.dart
+++ test/infra/api/clients/http_client_test.dart
@@ -88,5 +88,11 @@ void main() {
       await sut.get(url: url, queryString: { 'q1': 'v1', 'q2': 'v2' });
       expect(client.url, '$url?q1=v1&q2=v2');
     });
+
+    test('should request with correct queryStrings and params', () async {
+      url = 'http://anyurl.com/:p3/:p4';
+      await sut.get(url: url, queryString: { 'q1': 'v1', 'q2': 'v2' }, params: { 'p3': 'v3', 'p4': 'v4' });
+      expect(client.url, 'http://anyurl.com/v3/v4?q1=v1&q2=v2');
+    });
   });
 }
