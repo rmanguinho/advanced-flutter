039211fa2f5ef7e3c6bc7af45b9f059f359c6783
Rodrigo Manguinho <rodrigo.manguinho@gmail.com>
2024-06-04 17:33:27 -0300

    move HttpAdapter to its own file

---
+++ lib/infra/api/adapters/http_adapter.dart
@@ -0,0 +1,37 @@
+import 'dart:convert';
+
+import 'package:advanced_flutter/domain/entities/domain_error.dart';
+import 'package:advanced_flutter/infra/types/json.dart';
+
+import 'package:dartx/dartx.dart';
+import 'package:http/http.dart';
+
+class HttpAdapter {
+  final Client client;
+
+  HttpAdapter({
+    required this.client
+  });
+
+  Future<T?> get<T>({ required String url, Map<String, String>? headers, Map<String, String?>? params, Map<String, String>? queryString }) async {
+    final allHeaders = (headers ?? {})..addAll({ 'content-type': 'application/json', 'accept': 'application/json' });
+    final uri = _buildUri(url: url, params: params, queryString: queryString);
+    final response = await client.get(uri, headers: allHeaders);
+    switch (response.statusCode) {
+      case 200: {
+        if (response.body.isEmpty) return null;
+        final data = jsonDecode(response.body);
+        return (T == JsonArr) ? data.map<Json>((e) => e as Json).toList() : data;
+      }
+      case 204: return null;
+      case 401: throw DomainError.sessionExpired;
+      default: throw DomainError.unexpected;
+    }
+  }
+
+  Uri _buildUri({ required String url, Map<String, String?>? params, Map<String, String>? queryString }) {
+    url = params?.keys.fold(url, (result, key) => result.replaceFirst(':$key', params[key] ?? '')).removeSuffix('/') ?? url;
+    url = queryString?.keys.fold('$url?', (result, key) => '$result$key=${queryString[key]}&').removeSuffix('&') ?? url;
+    return Uri.parse(url);
+  }
+}
--- test/infra/api/adapters/http_adapter_test.dart
+++ test/infra/api/adapters/http_adapter_test.dart
@@ -1,48 +1,15 @@
-import 'dart:convert';
-
 import 'package:advanced_flutter/domain/entities/domain_error.dart';
+import 'package:advanced_flutter/infra/api/adapters/http_adapter.dart';
 import 'package:advanced_flutter/infra/types/json.dart';
 
-import 'package:dartx/dartx.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:http/http.dart';
 
 import '../../../helpers/fakes.dart';
-import 'client_spy.dart';
-
-class HttpClient {
-  final Client client;
-
-  HttpClient({
-    required this.client
-  });
-
-  Future<T?> get<T>({ required String url, Map<String, String>? headers, Map<String, String?>? params, Map<String, String>? queryString }) async {
-    final allHeaders = (headers ?? {})..addAll({ 'content-type': 'application/json', 'accept': 'application/json' });
-    final uri = _buildUri(url: url, params: params, queryString: queryString);
-    final response = await client.get(uri, headers: allHeaders);
-    switch (response.statusCode) {
-      case 200: {
-        if (response.body.isEmpty) return null;
-        final data = jsonDecode(response.body);
-        return (T == JsonArr) ? data.map<Json>((e) => e as Json).toList() : data;
-      }
-      case 204: return null;
-      case 401: throw DomainError.sessionExpired;
-      default: throw DomainError.unexpected;
-    }
-  }
-
-  Uri _buildUri({ required String url, Map<String, String?>? params, Map<String, String>? queryString }) {
-    url = params?.keys.fold(url, (result, key) => result.replaceFirst(':$key', params[key] ?? '')).removeSuffix('/') ?? url;
-    url = queryString?.keys.fold('$url?', (result, key) => '$result$key=${queryString[key]}&').removeSuffix('&') ?? url;
-    return Uri.parse(url);
-  }
-}
+import '../clients/client_spy.dart';
 
 void main() {
   late ClientSpy client;
-  late HttpClient sut;
+  late HttpAdapter sut;
   late String url;
 
   setUp(() {
@@ -54,7 +21,7 @@ void main() {
       }
     ''';
     url = anyString();
-    sut = HttpClient(client: client);
+    sut = HttpAdapter(client: client);
   });
 
   group('get', () {
