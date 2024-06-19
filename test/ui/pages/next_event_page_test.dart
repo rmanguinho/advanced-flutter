e067a4b063a89f7bc57432e76c0ec1943af55409
Rodrigo Manguinho <rodrigo.manguinho@gmail.com>
2024-06-19 18:30:33 -0300

    ensure NextEventPage hide goalkeepers section

--- test/ui/pages/next_event_page_test.dart
+++ test/ui/pages/next_event_page_test.dart
@@ -49,15 +49,36 @@ class _NextEventPageState extends State<NextEventPage> {
         builder: (context, snapshot) {
           if (snapshot.connectionState != ConnectionState.active) return const CircularProgressIndicator();
           if (snapshot.hasError) return const SizedBox();
+          final viewModel = snapshot.data!;
           return ListView(
             children: [
-              const Text('DENTRO - GOLEIROS'),
-              Text(snapshot.data!.goalkeepers.length.toString()),
-              ...snapshot.data!.goalkeepers.map((player) => Text(player.name))
+              if (viewModel.goalkeepers.isNotEmpty) ListSection(title: 'DENTRO - GOLEIROS', items: viewModel.goalkeepers)
             ]
           );
         }
       )
+    );
+  }
+}
+
+final class ListSection extends StatelessWidget {
+  final String title;
+  final List<NextEventPlayerViewModel> items;
+
+  const ListSection({
+    required this.title,
+    required this.items,
+    super.key
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Text(title),
+        Text(items.length.toString()),
+        ...items.map((player) => Text(player.name))
+      ]
     );
   }
 }
@@ -145,5 +166,12 @@ void main() {
     expect(find.text('Rodrigo'), findsOneWidget);
     expect(find.text('Rafael'), findsOneWidget);
     expect(find.text('Pedro'), findsOneWidget);
+  });
+
+  testWidgets('should hide all sections', (tester) async {
+    await tester.pumpWidget(sut);
+    presenter.emitNextEvent();
+    await tester.pump();
+    expect(find.text('DENTRO - GOLEIROS'), findsNothing);
   });
 }
