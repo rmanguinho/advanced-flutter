3a6de8ad519565b3b908e260c68706af1e46658c
Rodrigo Manguinho <rodrigo.manguinho@gmail.com>
2024-06-19 14:38:22 -0300

    move sut creation to setup

--- test/ui/pages/next_event_page_test.dart
+++ test/ui/pages/next_event_page_test.dart
@@ -46,10 +46,17 @@ final class NextEventPresenterSpy implements NextEventPresenter {
 }
 
 void main() {
+  late NextEventPresenterSpy presenter;
+  late String groupId;
+  late Widget sut;
+
+  setUp(() {
+    presenter = NextEventPresenterSpy();
+    groupId = anyString();
+    sut = MaterialApp(home: NextEventPage(presenter: presenter, groupId: groupId));
+  });
+
   testWidgets('should load event data on page init', (tester) async {
-    final presenter = NextEventPresenterSpy();
-    final groupId = anyString();
-    final sut = MaterialApp(home: NextEventPage(presenter: presenter, groupId: groupId));
     await tester.pumpWidget(sut);
     expect(presenter.loadCallsCount, 1);
     expect(presenter.groupId, groupId);
