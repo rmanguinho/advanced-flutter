9949e8ea8df6e3867dec2ca1f6f91eb3a2f32cf9
Rodrigo Manguinho <rodrigo.manguinho@gmail.com>
2024-06-19 18:35:28 -0300

    ensure NextEventPage presents doubt section

--- test/ui/pages/next_event_page_test.dart
+++ test/ui/pages/next_event_page_test.dart
@@ -8,11 +8,13 @@ final class NextEventViewModel {
   final List<NextEventPlayerViewModel> goalkeepers;
   final List<NextEventPlayerViewModel> players;
   final List<NextEventPlayerViewModel> out;
+  final List<NextEventPlayerViewModel> doubt;
 
   const NextEventViewModel({
     this.goalkeepers = const [],
     this.players = const [],
     this.out = const [],
+    this.doubt = const []
   });
 }
 
@@ -59,6 +61,7 @@ class _NextEventPageState extends State<NextEventPage> {
               if (viewModel.goalkeepers.isNotEmpty) ListSection(title: 'DENTRO - GOLEIROS', items: viewModel.goalkeepers),
               if (viewModel.players.isNotEmpty) ListSection(title: 'DENTRO - JOGADORES', items: viewModel.players),
               if (viewModel.out.isNotEmpty) ListSection(title: 'FORA', items: viewModel.out),
+              if (viewModel.doubt.isNotEmpty) ListSection(title: 'DÚVIDA', items: viewModel.doubt)
             ]
           );
         }
@@ -106,8 +109,8 @@ final class NextEventPresenterSpy implements NextEventPresenter {
     nextEventSubject.add(viewModel ?? const NextEventViewModel());
   }
 
-  void emitNextEventWith({ List<NextEventPlayerViewModel> goalkeepers = const [], List<NextEventPlayerViewModel> players = const [], List<NextEventPlayerViewModel> out = const [] }) {
-    nextEventSubject.add(NextEventViewModel(goalkeepers: goalkeepers, players: players, out: out));
+  void emitNextEventWith({ List<NextEventPlayerViewModel> goalkeepers = const [], List<NextEventPlayerViewModel> players = const [], List<NextEventPlayerViewModel> out = const [], List<NextEventPlayerViewModel> doubt = const [] }) {
+    nextEventSubject.add(NextEventViewModel(goalkeepers: goalkeepers, players: players, out: out, doubt: doubt));
   }
 
   void emitError() {
@@ -198,6 +201,21 @@ void main() {
     ]);
     await tester.pump();
     expect(find.text('FORA'), findsOneWidget);
+    expect(find.text('3'), findsOneWidget);
+    expect(find.text('Rodrigo'), findsOneWidget);
+    expect(find.text('Rafael'), findsOneWidget);
+    expect(find.text('Pedro'), findsOneWidget);
+  });
+
+  testWidgets('should present doubt section', (tester) async {
+    await tester.pumpWidget(sut);
+    presenter.emitNextEventWith(doubt: const [
+      NextEventPlayerViewModel(name: 'Rodrigo'),
+      NextEventPlayerViewModel(name: 'Rafael'),
+      NextEventPlayerViewModel(name: 'Pedro')
+    ]);
+    await tester.pump();
+    expect(find.text('DÚVIDA'), findsOneWidget);
     expect(find.text('3'), findsOneWidget);
     expect(find.text('Rodrigo'), findsOneWidget);
     expect(find.text('Rafael'), findsOneWidget);
