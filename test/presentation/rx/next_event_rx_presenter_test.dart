d8cbd66c395d23a1470be17dab45055917ffe0ed
Rodrigo Manguinho <rodrigo.manguinho@gmail.com>
2024-06-26 14:25:03 -0300

    ensure NextEventRxPresenter emits correct events on reload with success

--- test/presentation/rx/next_event_rx_presenter_test.dart
+++ test/presentation/rx/next_event_rx_presenter_test.dart
@@ -23,6 +23,7 @@ final class NextEventRxPresenter {
       await nextEventLoader(groupId: groupId);
     } catch (error) {
       nextEventSubject.addError(error);
+    } finally {
       if (isReload) isBusySubject.add(false);
     }
   }
@@ -69,5 +70,10 @@ void main() {
     expectLater(sut.nextEventStream, emitsError(nextEventLoader.error));
     sut.isBusyStream.listen(neverCalled);
     await sut.loadNextEvent(groupId: groupId);
+  });
+
+  test('should emit correct events on reload with success', () async {
+    expectLater(sut.isBusyStream, emitsInOrder([true, false]));
+    await sut.loadNextEvent(groupId: groupId, isReload: true);
   });
 }
