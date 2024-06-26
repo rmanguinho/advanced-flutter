2fc70f540a6b02936d2539966ed618dc1d72781f
Rodrigo Manguinho <rodrigo.manguinho@gmail.com>
2024-06-26 14:19:23 -0300

    ensure NextEventRxPresenter emits correct events on reload with error

--- test/presentation/rx/next_event_rx_presenter_test.dart
+++ test/presentation/rx/next_event_rx_presenter_test.dart
@@ -1,26 +1,42 @@
+@Timeout(Duration(seconds: 1))
+
 import 'package:flutter_test/flutter_test.dart';
+import 'package:rxdart/rxdart.dart';
 
 import '../../helpers/fakes.dart';
 
 final class NextEventRxPresenter {
   final Future<void> Function({ required String groupId }) nextEventLoader;
+  final nextEventSubject = BehaviorSubject();
+  final isBusySubject = BehaviorSubject<bool>();
 
-  const NextEventRxPresenter({
+  NextEventRxPresenter({
     required this.nextEventLoader
   });
 
-  Future<void> loadNextEvent({ required String groupId }) async {
-    await nextEventLoader(groupId: groupId);
+  Stream get nextEventStream => nextEventSubject.stream;
+  Stream<bool> get isBusyStream => isBusySubject.stream;
+
+  Future<void> loadNextEvent({ required String groupId, bool isReload = false }) async {
+    try {
+      isBusySubject.add(true);
+      await nextEventLoader(groupId: groupId);
+    } catch (error) {
+      nextEventSubject.addError(error);
+      isBusySubject.add(false);
+    }
   }
 }
 
 final class NextEventLoaderSpy {
   int callsCount = 0;
   String? groupId;
+  Error? error;
 
   Future<void> call({ required String groupId }) async {
     this.groupId = groupId;
     callsCount++;
+    if (error != null) throw error!;
   }
 }
 
@@ -39,5 +55,12 @@ void main() {
     await sut.loadNextEvent(groupId: groupId);
     expect(nextEventLoader.callsCount, 1);
     expect(nextEventLoader.groupId, groupId);
+  });
+
+  test('should emit correct events on reload with error', () async {
+    nextEventLoader.error = Error();
+    expectLater(sut.nextEventStream, emitsError(nextEventLoader.error));
+    expectLater(sut.isBusyStream, emitsInOrder([true, false]));
+    await sut.loadNextEvent(groupId: groupId, isReload: true);
   });
 }
