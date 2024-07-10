8ec20a2a7e03aa5879eca3fb97c2d8c5811f5f16
Rodrigo Manguinho <rodrigo.manguinho@gmail.com>
2024-07-10 12:14:16 -0300

    remove delay

--- lib/infra/api/repositories/load_next_event_api_repo.dart
+++ lib/infra/api/repositories/load_next_event_api_repo.dart
@@ -13,7 +13,6 @@ final class LoadNextEventApiRepository {
   });
 
   Future<NextEvent> loadNextEvent({ required String groupId }) async {
-    await Future.delayed(const Duration(seconds: 2));
     final json = await httpClient.get(url: url, params: { "groupId": groupId });
     if (json == null) throw UnexpectedError();
     return NextEventMapper().toObject(json);
