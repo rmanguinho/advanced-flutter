8c31ee1b7cb3dc57191ec741b5763ffcda9735bd
Rodrigo Manguinho <rodrigo.manguinho@gmail.com>
2024-07-10 12:15:00 -0300

    fix button test case

--- test/ui/pages/next_event_page_test.dart
+++ test/ui/pages/next_event_page_test.dart
@@ -146,7 +146,7 @@ void main() {
     expect(find.byType(PlayerStatus), findsNothing);
     expect(find.byType(PlayerPhoto), findsNothing);
     expect(find.text('Algo errado aconteceu, tente novamente.'), findsOneWidget);
-    expect(find.text('Recarregar'), findsOneWidget);
+    expect(find.text('RECARREGAR'), findsOneWidget);
   });
 
   testWidgets('should load event data on reload click', (tester) async {
@@ -156,7 +156,7 @@ void main() {
     expect(presenter.isReload, false);
     presenter.emitError();
     await tester.pump();
-    await tester.tap(find.text('Recarregar'));
+    await tester.tap(find.text('RECARREGAR'));
     expect(presenter.callsCount, 2);
     expect(presenter.groupId, groupId);
     expect(presenter.isReload, true);
