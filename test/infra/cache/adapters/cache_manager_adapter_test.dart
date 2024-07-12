8e28cf2317e54f98aac8981ef8fad96537a8f94c
Rodrigo Manguinho <rodrigo.manguinho@gmail.com>
2024-07-12 17:18:19 -0300

    make CacheManagerAdapter implements CacheGetClient

--- test/infra/cache/adapters/cache_manager_adapter_test.dart
+++ test/infra/cache/adapters/cache_manager_adapter_test.dart
@@ -1,18 +1,20 @@
 import 'dart:convert';
 
+import 'package:advanced_flutter/infra/cache/clients/cache_get_client.dart';
 import 'package:flutter_cache_manager/flutter_cache_manager.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 import '../../../mocks/fakes.dart';
 import '../mocks/cache_manager_spy.dart';
 
-final class CacheManagerAdapter {
+final class CacheManagerAdapter implements CacheGetClient {
   final BaseCacheManager client;
 
   const CacheManagerAdapter({
     required this.client
   });
 
+  @override
   Future<dynamic> get({ required String key }) async {
     try {
       final info = await client.getFileFromCache(key);
