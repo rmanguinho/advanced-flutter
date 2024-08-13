a821da384a9009d25918139fcd3779ad031cf52f
Rodrigo Manguinho <rodrigo.manguinho@gmail.com>
2024-08-13 16:26:32 -0300

    change presenter factory to use composite repo

--- lib/main/factories/ui/pages/next_event_page_factory.dart
+++ lib/main/factories/ui/pages/next_event_page_factory.dart
@@ -1,11 +1,11 @@
-import 'package:advanced_flutter/main/factories/infra/api/repositories/load_next_event_api_repo_factory.dart';
+import 'package:advanced_flutter/main/factories/infra/repositories/load_next_event_from_api_with_cache_fallback_repo_factory.dart';
 import 'package:advanced_flutter/presentation/rx/next_event_rx_presenter.dart';
 import 'package:advanced_flutter/ui/pages/next_event_page.dart';
 
 import 'package:flutter/material.dart';
 
 Widget makeNextEventPage() {
-  final repo = makeLoadNextEventApiRepository();
+  final repo = makeLoadNextEventFromApiWithCacheFallbackRepository();
   final presenter = NextEventRxPresenter(nextEventLoader: repo.loadNextEvent);
   return NextEventPage(presenter: presenter, groupId: 'valid_id');
 }
