43a8974df2b1eb4e53d4bc253b46ac9a8606de3f
Rodrigo Manguinho <rodrigo.manguinho@gmail.com>
2025-02-06 18:54:48 -0300

    ensure AuthorizedHttpGetClient calls HttpClient with correct input

--- test/infra/api/clients/authorized_http_get_client_test.dart
+++ test/infra/api/clients/authorized_http_get_client_test.dart
@@ -1,33 +1,56 @@
+import 'package:advanced_flutter/infra/api/clients/http_get_client.dart';
 import 'package:advanced_flutter/infra/cache/clients/cache_get_client.dart';
+import 'package:advanced_flutter/infra/types/json.dart';
 
 import 'package:flutter_test/flutter_test.dart';
 
+import '../../../mocks/fakes.dart';
 import '../../cache/mocks/cache_get_client_spy.dart';
+import '../mocks/http_get_client_spy.dart';
 
 final class AuthorizedHttpGetClient {
   final CacheGetClient cacheClient;
+  final HttpGetClient httpClient;
 
   const AuthorizedHttpGetClient({
-    required this.cacheClient
+    required this.cacheClient,
+    required this.httpClient
   });
 
-  Future<void> get() async {
+  Future<void> get({ required String url, Json? params, Json? queryString }) async {
     await cacheClient.get(key: 'current_user');
+    await httpClient.get(url: url, params: params, queryString: queryString);
   }
 }
 
 void main() {
   late CacheGetClientSpy cacheClient;
+  late HttpGetClientSpy httpClient;
   late AuthorizedHttpGetClient sut;
+  late String url;
+  late Json params;
+  late Json queryString;
 
   setUp(() {
+    url = anyString();
+    params = anyJson();
+    queryString = anyJson();
     cacheClient = CacheGetClientSpy();
-    sut = AuthorizedHttpGetClient(cacheClient: cacheClient);
+    httpClient = HttpGetClientSpy();
+    sut = AuthorizedHttpGetClient(cacheClient: cacheClient, httpClient: httpClient);
   });
 
   test('should call CacheClient with correct input', () async {
-    await sut.get();
+    await sut.get(url: url);
     expect(cacheClient.callsCount, 1);
     expect(cacheClient.key, 'current_user');
+  });
+
+  test('should call HttpClient with correct input', () async {
+    await sut.get(url: url, params: params, queryString: queryString);
+    expect(httpClient.callsCount, 1);
+    expect(httpClient.url, url);
+    expect(httpClient.params, params);
+    expect(httpClient.queryString, queryString);
   });
 }
