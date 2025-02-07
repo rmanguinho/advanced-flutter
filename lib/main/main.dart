f5f78319b1b23f4f4f55c262bee69c99554b2d19
Rodrigo Manguinho <rodrigo.manguinho@gmail.com>
2025-02-07 16:44:55 -0300

    populate cache with user access token

--- lib/main/main.dart
+++ lib/main/main.dart
@@ -1,8 +1,11 @@
+import 'package:advanced_flutter/main/factories/infra/cache/adapters/cache_manager_adapter_factory.dart';
 import 'package:advanced_flutter/main/factories/ui/pages/next_event_page_factory.dart';
 
 import 'package:flutter/material.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await makeCacheManagerAdapter().save(key: 'current_user', value: { 'accessToken': 'valid_token' });
   runApp(const MyApp());
 }
 
